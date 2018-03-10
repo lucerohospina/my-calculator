08736ec48da2a67bea184651e3b3bb201c5fd173
eliza29 <elizaseguraf@gmail.com>
2018-03-10 13:08:13 -0500

    avances css

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -3,20 +3,20 @@ import React, { Component } from 'react';
 class Keyboard extends Component {
   render() {
     const firstRow = this.props.numbers.map((number, index) => {
-      return <span key={index} class="box-span">{number}</span>
+      return <span key={index} className="box-span">{number}</span>
     })
     const secondRow = this.props.numbers2.map((number, index) => {
-      return <span key={index} class="box-span">{number}</span>
+      return <span key={index} className="box-span">{number}</span>
     })
     const thirdRow = this.props.numbers3.map((number, index) => {
-      return <span key={index} class="box-span">{number}</span>
+      return <span key={index} className="box-span">{number}</span>
     })
-    const fourthRow = this.props.numbers3.map((number, index) => {
-      return <span key={index} class="box-span">{number}</span>
+    const fourthRow = this.props.numbers4.map((number, index) => {
+      return <span key={index} className="box-span2 font-weight-bold">{number}</span>
     })
 
     return (
-      <div>
+      <div className = "ml-2">
         <div>
           <span>{firstRow}</span>
         </div>
