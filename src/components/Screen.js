e5edcec4c017bacd803d8dadaf17655f80c5fb17
eliza29 <elizaseguraf@gmail.com>
2018-03-10 11:20:11 -0500

    subiendo script

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,5 +1,18 @@
 import React, { Component } from 'react';
 
 
-
+class Screen extends Component {
+    render(){
+      const {num} = this.props
+        
+        return (
+             
+               <div className="">
+                   {num}
+               </div>
+           
+        )
+    }
+   }
+   
 export default Screen;
